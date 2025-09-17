2cd2d2283f1a730ad5b73365c5c17b896e159e0e
Kacper <kgrzesik17@gmail.com>
2025-09-17 09:21:48 +0200

    practice + mobile nav

--- website1/shared.js
+++ website1/shared.js
@@ -1,5 +1,21 @@
 var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var modalButton = document.querySelector('.modal .modal__action--negative')
 
-// console.dir(backdrop);
+for(var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    })
+}
 
-backdrop.style.display = 'block';
+backdrop.addEventListener('click', function() {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+})
+
+modalButton.addEventListener('click', function() {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+})
